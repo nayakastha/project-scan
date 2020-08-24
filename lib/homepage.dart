588b90a421ffe1d0eef74662f684c9c591c5d93a
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2020-08-24 11:37:21 +0530

    work on "Remove" Button

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,6 +6,7 @@ import 'package:qrscan/qrscan.dart' as scanner;
 import 'dart:io';
 import 'dart:typed_data';
 
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -36,22 +37,139 @@ class _HomePageState extends State<HomePage> {
         return ListView(
           children: <Widget>[
             Container(
-              color: Color.fromRGBO(255, 250, 250, 1),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Colors.blue, Colors.red])
+              ),
+              //color: Color.fromRGBO(255, 250, 250, 1),
               child: Column(
                 children: <Widget>[
                   SizedBox(
                     height: 5,
                   ),
-                  _qrWidget(this.ss, context),
                   Padding(
-                    padding: const EdgeInsets.all(10.0),
+                    padding: EdgeInsets.all(20),
+                    child: Card(
+                      elevation: 6,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Icon(Icons.verified_user,
+                                    size: 18, color: Colors.green),
+                                Text(' Generated QR Code',
+                                    style: TextStyle(fontSize: 18)),
+                                Spacer(),
+                              ],
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.black12,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(4),
+                                  topRight: Radius.circular(4)),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                                left: 20, right: 20, top: 20, bottom: 20),
+                            child: Column(
+                              children: <Widget>[
+                                SizedBox(
+                                  height: 190,
+                                  child: ss.isEmpty
+                                      ? Center(
+                                          child: Text(
+                                              'The Generated QR Code displays here',
+                                              style: TextStyle(
+                                                  color: Colors.black38)),
+                                        )
+                                      : Image.memory(ss),
+                                ),
+                                Divider(
+                                  height: 1,
+                                  color: Colors.black26,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      top: 7, left: 25, right: 25),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                      InkWell(
+                                          child: Container(
+                                            margin: EdgeInsets.only(right: 25),
+                                            child: Text(
+                                              'remove',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.blue),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                          onTap: () {
+                                            setState(() {
+                                              ss = Uint8List(0);
+                                            });
+                                          }),
+                                      SizedBox(
+                                        width: 20,
+                                      ),
+                                      InkWell(
+                                        onTap: () async {
+                                          final success =
+                                              await ImageGallerySaver.saveImage(
+                                                  ss);
+                                          SnackBar snackBar;
+                                          if (success) {
+                                            snackBar = new SnackBar(
+                                                content: new Text(
+                                                    'Successful Preservation!'));
+                                            Scaffold.of(context)
+                                                .showSnackBar(snackBar);
+                                          } else {
+                                            snackBar = new SnackBar(
+                                                content:
+                                                    new Text('Save failed!'));
+                                          }
+                                        },
+                                        child: Text(
+                                          'save',
+                                          style: TextStyle(
+                                              fontSize: 15, color: Colors.blue),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Divider(height: 1, color: Colors.black26),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left:20.0, right: 20.0),
                     child: TextField(
                       controller: this._inputController,
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.go,
                       onSubmitted: (value) async {
                         Uint8List result = await scanner.generateBarCode(value);
-                        this.setState(() => this.ss = result);
+                        setState(() => ss = result);
                       },
                       decoration: InputDecoration(
                         filled: true,
@@ -64,8 +182,8 @@ class _HomePageState extends State<HomePage> {
                         ),
                         helperText: 'Data for the generation of QRCode',
                         helperStyle: TextStyle(color: Colors.black),
-                        labelText: 'Please Input Your Data for QR Code',
-                        labelStyle: TextStyle(
+                        hintText: 'Please Input Your Data for QR Code',
+                        hintStyle: TextStyle(
                           fontSize: 15,
                         ),
                         contentPadding:
@@ -74,14 +192,14 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   SizedBox(
-                    height: 8.0,
+                    height: 15.0,
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.only(left:20.0,right: 20.0),
                     child: TextField(
                       controller: this._outputController,
                       readOnly: true,
-                      maxLines: 2,
+                      //maxLines: 2,
                       decoration: InputDecoration(
                         filled: true,
                         fillColor: Colors.white,
@@ -93,8 +211,8 @@ class _HomePageState extends State<HomePage> {
                         ),
                         helperText: 'The Scanned Result',
                         helperStyle: TextStyle(color: Colors.black),
-                        labelText: 'The results after scanning are :',
-                        labelStyle: TextStyle(fontSize: 16),
+                        hintText: 'The results after scanning are :',
+                        hintStyle: TextStyle(fontSize: 16),
                         contentPadding:
                             EdgeInsets.symmetric(horizontal: 8, vertical: 16),
                       ),
@@ -102,7 +220,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                   SizedBox(height: 20),
                   _buttongroup(_inputController, _outputController, ss),
-                  SizedBox(height: 90)
+                  SizedBox(height: 150)
                 ],
               ),
             ),
@@ -111,100 +229,6 @@ class _HomePageState extends State<HomePage> {
       }),
     ));
   }
-}
-
-Widget _qrWidget(Uint8List ss, BuildContext context) {
-  return Padding(
-    padding: EdgeInsets.all(20),
-    child: Card(
-      elevation: 6,
-      child: Column(
-        children: <Widget>[
-          Container(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Icon(Icons.verified_user, size: 18, color: Colors.green),
-                Text(' Generated QR Code', style: TextStyle(fontSize: 18)),
-                Spacer(),
-              ],
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(4), topRight: Radius.circular(4)),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 190,
-                  child: ss.isEmpty
-                      ? Center(
-                          child: Text('The Generated QR Code displays here',
-                              style: TextStyle(color: Colors.black38)),
-                        )
-                      : Image.memory(ss),
-                ),
-                Divider(height: 1, color: Colors.black26,),
-                Padding(
-                  padding: EdgeInsets.only(top: 7, left: 25, right: 25),
-                  child: Row(
-
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-
-                         InkWell(
-                            child: Container(
-                              margin: EdgeInsets.only(right: 25),
-                              child: Text(
-                                'remove',
-                                style:
-                                    TextStyle(fontSize: 15, color: Colors.blue),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            onTap: () {
-                              ss = Uint8List(0);
-                            }),
-                       SizedBox(width: 20,),
-
-                       InkWell(
-                          onTap: () async {
-                            final success =
-                                await ImageGallerySaver.saveImage(ss);
-                            SnackBar snackBar;
-                            if (success) {
-                              snackBar = new SnackBar(
-                                  content:
-                                      new Text('Successful Preservation!'));
-                              Scaffold.of(context).showSnackBar(snackBar);
-                            } else {
-                              snackBar = new SnackBar(
-                                  content: new Text('Save failed!'));
-                            }
-                          },
-                          child: Text(
-                            'save',
-                            style: TextStyle(fontSize: 15, color: Colors.blue),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-          Divider(height: 1, color: Colors.black26),
-        ],
-      ),
-    ),
-  );
 }
 
 Widget _buttongroup(TextEditingController _inputController,
@@ -240,7 +264,7 @@ Widget _buttongroup(TextEditingController _inputController,
           style: TextStyle(color: Colors.white),
         ),
         icon: Icon(
-          Icons.photo_album,
+          Icons.photo,
           color: Colors.white,
         ),
         backgroundColor: Colors.blue[300],
